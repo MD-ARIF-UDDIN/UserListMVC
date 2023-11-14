fce4990760c8e89d0c8634e567056bb8a2d790ea
MD Arif <leoarifuddin@gmail.com>
2023-11-14 17:34:38 +0600

    Add project files.

---
+++ UserListMVC/Controllers/UsersController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserListMVC.Models;
+
+namespace UserListMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public User User { get; set; }
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            User = new User();
+            if (id == null)
+            {
+                return View(User);
+            }
+
+            User = _db.Users.FirstOrDefault(u => u.Id == id);
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return View(User);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (User.Id == 0)
+                {
+                    _db.Users.Add(User);
+                }
+                else
+                {
+                    _db.Users.Update(User);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(User);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Users.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var userFromDb = await _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (userFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Users.Remove(userFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ UserListMVC/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserListMVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
